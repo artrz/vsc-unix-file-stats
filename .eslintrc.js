53acea313bac646e28aecfe6566ddb7e0d6e6a4b
Arturo Rodríguez <arturo@fugadigital.com>
2024-07-24 19:32:07 -0600

    Add new config files

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {
+    },
+}
