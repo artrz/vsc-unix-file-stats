f505ea8e090105aa3d973dc664961cd81acc36f1
Arturo Rodríguez <arturo@fugadigital.com>
2024-07-26 18:13:30 -0600

    Add and configure stylistic/eslint-plugin dev dep

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,40 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const stylistic = require('@stylistic/eslint-plugin');
+
+const customStyle = stylistic.configs.customize({
+    arrowParens: true,
+    blockSpacing: true,
+    braceStyle: 'stroustrup',
+    commaDangle: 'always-multiline',
+    indent: 4,
+    jsx: false,
+    quoteProps: 'consistent-as-needed',
+    semi: true,
+});
+
 module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
     parser: '@typescript-eslint/parser',
     parserOptions: {
+        ecmaVersion: 6,
         sourceType: 'module',
     },
-    plugins: [
-        '@typescript-eslint',
+    plugins: ['@typescript-eslint', '@stylistic'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
     rules: {
+        'curly': 'error',
+        'eqeqeq': 'error',
+        'no-throw-literal': 'error',
+        ...customStyle.rules,
+        '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+        '@stylistic/wrap-iife': ['error', 'inside', { functionPrototypeMethods: true }],
     },
-}
+    ignorePatterns: ['out', 'dist', '**/*.d.ts'],
+};
