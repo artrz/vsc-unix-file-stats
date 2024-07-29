aa567352bd871b5cf2613e7b2dc7c094d88ee475
Arturo Rodríguez <arturo@fugadigital.com>
2024-07-29 17:18:40 -0600

    ci: add and configure commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,14 @@
+export default { extends: ['@commitlint/config-conventional'] };
+
+// VALID TYPES:
+//   - build
+//   - chore
+//   - ci
+//   - docs
+//   - feat
+//   - fix
+//   - perf
+//   - refactor
+//   - revert
+//   - style
+//   - test
